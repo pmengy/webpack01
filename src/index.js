b8bf1757091f799b42b9cb93e59eeef1e1ef80a5
TTpeach1 <TTpeach1@Outlook.com>
2022-06-27 20:40:08 +0800

    打包更新

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// 轮播图逻辑
+// console.log('实现轮播图的业务逻辑')
+// // tab栏切换的逻辑
+// console.log('实现tabs标签页的逻辑')
+
+import './banner.js'
+import './tabs.js'
+
+import $ from 'jquery';
+$('#swiper').css('background-color', 'pink')
