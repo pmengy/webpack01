3d844a0e90d8dc3fbd6179ffd36e457c5b0a1b4f
TTpeach1 <TTpeach1@Outlook.com>
2022-06-27 21:24:57 +0800

    处理高版本js语法

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@
 // // tab栏切换的逻辑
 // console.log('实现tabs标签页的逻辑')
 
-import './banner.js'
-import './tabs.js'
+import './banner.js';
+import './tabs.js';
 
 import $ from 'jquery';
 // $('#swiper').css('background-color', 'pink')
 
-import './style/index.css'
+import './style/index.css';
 import './style/index.less';
 
 // index.js
@@ -19,10 +19,16 @@ img.src = imgUrl;
 document.body.appendChild(img);
 
 // 引入图片-使用
-import imgUrl1 from './assets/logo_small.png'
-const theImg = document.createElement('img')
-theImg.src = imgUrl1
-document.body.appendChild(theImg)
+import imgUrl1 from './assets/logo_small.png';
+const theImg = document.createElement('img');
+theImg.src = imgUrl1;
+document.body.appendChild(theImg);
 
 // 引入字体图标文件
-import './assets/fonts/iconfont.css'
+import './assets/fonts/iconfont.css';
+
+class App {
+  static a = 123;
+}
+
+console.log(App.a);
