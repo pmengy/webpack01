548268f440b1271a85a249c190284dca4f1fb0b6
pmengy <936841379@qq.com>
2022-06-27 20:48:17 +0800

    解决css和less文件打包问题

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 // import './app.vue';
+// 引入js模块
 import './banner.js';
 import './tabs.js';
+// 引入css
+// import './styles/index.css'
+
+// 引入less
+import './styles/index.less'
--- webpack.config.js
+++ webpack.config.js
@@ -24,4 +24,83 @@ module.exports = {
     port: 3000, // 端口号
     open: true, // 自动打开
   },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }, // 匹配所有的css文件
+      {
+        test: /\.less$/, // 匹配执行类型的文件
+        // 使用less-loader, 让webpack处理less文件, 内置还会用less翻译less代码成css内容
+        // 执行的顺序 less-loader css-loader style-loader
+        // less-loader 先把less代码转换成css
+        // css-loader 再把css代码转换成webpack 可以识别的js代码
+        // style-loader 在把css代码插入到 dom中
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      // webpack 4
+      // {
+      //   test: /\.(png|jpg|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: 'url-loader', // 匹配文件, 尝试转base64字符串打包到js中
+      //       // 配置limit, 超过8k, 不转, file-loader复制, 随机名, 输出文件
+      //       options: {
+      //         limit: 8 * 1024,
+      //       },
+      //     },
+      //   ],
+      // },
+
+      // 图片文件的配置(仅适用于webpack5版本)
+      //   {
+      //     test: /\.(png|jpg|gif|jpeg)$/i,
+      //     // 如果你设置的是asset模式
+      //     // 以8KB大小区分图片文件
+      //     // 小于8KB的, 把图片文件转base64, 打包进js中
+      //     // 大于8KB的, 直接把图片文件输出到dist下
+
+      //     // type: 'asset/resource' // 发送一个单独的文件并导出 URL
+      //     // type: 'asset/inline' // 导出一个资源的 data URI
+      //     type: 'asset',
+      //     parser: {
+      //       dataUrlCondition: {
+      //         maxSize: 8 * 1024,
+      //       },
+      //     },
+      //     generator: {
+      //       // 生成器
+      //       filename: '[hash:6][ext]',
+      //       // [hash:6] 取哈希值前6位
+      //       // ext 文件拓展名
+      //     },
+      //   },
+
+      //   {
+      //     // webpack5默认内部不认识这些文件, 所以当做静态资源直接输出即可
+      //     test: /\.(eot|svg|ttf|woff|woff2)$/,
+      //     type: 'asset/resource',
+      //     generator: {
+      //       filename: 'font-[name].[hash:6][ext]',
+      //     },
+      //     parser: {
+      //       // 解析器 规则
+      //       dataUrlCondition: {
+      //         // dataUrl的情况
+      //         maxSize: 1 * 1024,
+      //         // maxSize 限制最大值
+      //       },
+      //     },
+      //   },
+
+      //   {
+      //     test: /\.js$/,
+      //     exclude: /(node_modules)/,
+      //     use: {
+      //       loader: 'babel-loader',
+      //       options: {
+      //         presets: ['@babel/preset-env'], // 预设:转码规则(用bable开发环境本来预设的)
+      //       },
+      //     },
+      //   },
+    ],
+  },
 };
