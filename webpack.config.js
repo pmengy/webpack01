fad8e0e336555410d91e1d571a584c9707b07ae8
TTpeach1 <TTpeach1@Outlook.com>
2022-06-27 20:49:13 +0800

    自动生成html文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require("path")
+// 引入自动生成 html 的插件
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: "./src/main.js", // 入口
     output: { 
         path: path.resolve(__dirname, "lib"), // 出口路径 绝对路径
         filename: "index.js" // 出口文件名
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html', // 告诉webpack使用插件时, 以我们自己的html文件作为模板去生成dist/html文件
+            filename: 'index.html' // 生成文件的名称
+          })
+    ],
 }
 // __dirname 可以用来动态获取当前文件所属目录的绝对路径
 // /Users/maohuihui/Desktop/vue-base/code/day_01/03_webpack配置_修改入口和出口
