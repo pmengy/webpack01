b473fd639ae25c2ad921fed304a5cecfbc560565
heimawl <1785969728@qq.com>
2022-06-27 20:56:15 +0800

    完成css/less文件问题

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import "./banner.js";
 import "./tabs.js";
+import './styles/index.css'
+import './styles/index.less';
 import $ from "jquery";
-$("#swiper").css("background-color", "red");
+// $("#swiper").css("background-color", "red");
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,28 @@ module.exports = {
   ],
   devServer: {
     port: 3000, // 端口号
-    open: true
+    open: true,
+  },
+  module: {
+    // loader 加载器 配置在这儿
+    rules: [
+      // loader的规则
+      {
+        test: /\.css$/, // 匹配所有的css文件
+        // loader 执行的顺序： use数组里从右向左运行
+        // 先用 css-loader 让webpack能够识别 css 文件的内容并打包
+        // 再用 style-loader 将样式, 把css插入到dom中
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.less$/, // 匹配执行类型的文件
+        // 使用less-loader, 让webpack处理less文件, 内置还会用less翻译less代码成css内容
+        // 执行的顺序 less-loader css-loader style-loader
+        // less-loader 先把less代码转换成css
+        // css-loader 再把css代码转换成webpack 可以识别的js代码
+        // style-loader 在把css代码插入到 dom中
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+    ],
   },
 };
